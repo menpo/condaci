1d17f888548f2247a7c529a6a1b99f2bc28dd050
James Booth <jabooth@gmail.com>
2014-09-01 23:06:56 +0100

    much improved purging (per file)

--- traviconda.py
+++ traviconda.py
@@ -5,14 +5,12 @@ import os.path as p
 from functools import partial
 import platform as stdplatform
 
-platform = stdplatform.system()
-
 
 def detect_arch():
     arch = stdplatform.architecture()[0]
     # need to be a little more sneaky to check the platform on Windows:
     # http://stackoverflow.com/questions/2208828/detect-64bit-os-windows-in-python
-    if platform == 'Windows':
+    if host_platform == 'Windows':
         if 'APPVEYOR' in os.environ:
             av_platform = os.environ['PLATFORM']
             if av_platform == 'x86':
@@ -23,10 +21,12 @@ def detect_arch():
                 print('Was unable to interpret the platform "{}"'.format())
     return arch
 
-arch = detect_arch()
+host_platform = stdplatform.system()
+host_arch = detect_arch()
+
 
 # define our commands
-if platform == 'Windows':
+if host_platform == 'Windows':
     script_dir_name = 'Scripts'
     miniconda_installer_path = 'C:\miniconda.exe'
     miniconda_dir = p.expanduser('C:\Miniconda')
@@ -35,15 +35,15 @@ else:
     miniconda_installer_path = p.expanduser('~/miniconda.sh')
     miniconda_dir = p.expanduser('~/miniconda')
 
-# Amazingly, adding these causes conda-build to fail parsing yaml. :-|
-#print('running on {} {}'.format(platform, arch))
-#print('miniconda_installer_path is {}'.format(miniconda_installer_path))
-#print('miniconda will be installed to {}'.format(miniconda_dir))
-
 miniconda_script_dir = p.join(miniconda_dir, script_dir_name)
 conda = p.join(miniconda_script_dir, 'conda')
 binstar = p.join(miniconda_script_dir, 'binstar')
 python = 'python'
+
+# Amazingly, adding these causes conda-build to fail parsing yaml. :-|
+#print('running on {} {}'.format(platform, arch))
+#print('miniconda_installer_path is {}'.format(miniconda_installer_path))
+#print('miniconda will be installed to {}'.format(miniconda_dir))
 
 
 def url_for_platform_version(platform, py_version, arch):
@@ -99,7 +99,7 @@ def download_file(url, path_to_download):
         fp.write(f.read())
 
 
-# BINSTAR  LOGIN
+# BINSTAR LOGIN
 
 def login():
     from binstar_client.utils import get_binstar
@@ -117,56 +117,96 @@ def login_with_key(key):
 
 # BINSTAR FILE PURGING
 
-from collections import namedtuple
 
-Binstar = namedtuple('Binstar', ['name', 'version', 'platform', 'filename'])
-platform_from_filename = lambda fn: fn.split('-')[-1].split('.')[0]
+class BinstarFile(object):
+
+    def __init__(self, full_name):
+        self.full_name = full_name
+
+    @property
+    def user(self):
+        return self.full_name.split('/')[0]
+
+    @property
+    def name(self):
+        return self.full_name.split('/')[1]
+
+    @property
+    def basename(self):
+        return '/'.join(self.full_name.split('/')[3:])
+
+    @property
+    def version(self):
+        return self.full_name.split('/')[2]
+
+    @property
+    def platform(self):
+        return self.full_name.replace('\\', '/').split('/')[3]
+
+    @property
+    def configuration(self):
+        return self.full_name.replace('\\', '/').split('/')[4].split('-')[2].split('.')[0]
+
+    def __str__(self):
+        return self.full_name
+
+    def __repr__(self):
+        return self.full_name
+
+    def all_info(self):
+        s = ["         user: {}".format(self.user),
+             "         name: {}".format(self.name),
+             "     basename: {}".format(self.basename),
+             "      version: {}".format(self.version),
+             "     platform: {}".format(self.platform),
+             "configuration: {}".format(self.configuration)]
+        return "\n".join(s)
+
+
+configuration_from_filename = lambda fn: fn.split('-')[-1].split('.')[0]
 name_from_filename = lambda fn: fn.split('-')[0]
 version_from_filename = lambda fn: fn.split('-')[1]
+platform_from_filepath = lambda fp: p.split(p.split(fp)[0])[-1]
+version_is_tag = lambda v: '_' not in v
 
 
-def version_is_tag(version):
-    return '_' not in version
+def channels_for_user(b, user):
+    return b.list_channels(user).keys()
 
 
-def all_files_on_channel(key, user, channel):
-    x = subprocess.check_output([binstar, '-t', key, 'channel',
-                                 '-o', user, '--show', channel])
-    return [Binstar(*y[4:].replace('\\', '/').split('/')[1:])
-            for y in x.split('\n')[1:-1]]
+def files_on_channel(b, user, channel):
+    info = b.show_channel(channel, user)
+    return [BinstarFile(i['full_name']) for i in info['files']]
 
 
-def all_tagged_versions(key, user, channel):
-    return [x for x in all_files_on_channel(key, user, channel)
-            if version_is_tag(x.version)]
+def remove_file(b, bfile):
+    b.remove_dist(bfile.user, bfile.name, bfile.version, bfile.basename)
 
 
-def all_non_tagged_versions(key, user, channel):
-    return [x for x in all_files_on_channel(key, user, channel)
-            if not version_is_tag(x.version)]
-
-
-def remove_all(b, to_purge):
-    for x in to_purge:
-        print('removing {}/{}/{}'.format(*x))
-        #b.remove_release(*x)
-
-
-def releases_to_remove(key, user, channel, filename):
+def files_to_remove(b, user, channel, filepath):
+    platform_ = platform_from_filepath(filepath)
+    filename = p.split(filepath)[-1]
     name = name_from_filename(filename)
     version = version_from_filename(filename)
-    all_files = all_files_on_channel(key, user, channel)
-    files_of_self = [f for f in all_files if f.name == name]
-    other_versions_of_self = [f for f in all_files
-                              if f.version != version and
-                              not version_is_tag(f.version)]
-    return other_versions_of_self
+    configuration = configuration_from_filename(filename)
+    # find all the files on this channel
+    all_files = files_on_channel(b, user, channel)
+    # other versions of this exact setup that are not tagged versions should
+    # be removed
+    return [f for f in all_files if
+            f.name == name and
+            f.configuration == configuration and
+            f.platform == platform_ and
+            f.version != version and
+            not version_is_tag(f.version)]
 
 
-def purge_old_releases(b, key, user, channel, filename):
-    to_remove = releases_to_remove(key, user, channel, filename)
-    print("Found {} releases to remove: {}".format(len(to_remove), to_remove))
-    remove_all(b, to_remove)
+def purge_old_files(b, user, channel, filepath):
+    to_remove = files_to_remove(b, user, channel, filepath)
+    print("Found {} releases to remove".format(len(to_remove)))
+    for old_file in to_remove:
+        print("Removing '{}'".format(old_file))
+        remove_file(b, old_file)
 
 
 # TRAVICONDA CONVIENIENCE FUNCTIONS
@@ -179,7 +219,7 @@ def acquire_miniconda(url, path_to_download):
 
 def install_miniconda(path_to_installer, path_to_install):
     print('Installing miniconda to {}'.format(path_to_install))
-    if platform == 'Windows':
+    if host_platform == 'Windows':
         execute([path_to_installer, '/S', '/D={}'.format(path_to_install)])
     else:
         execute(['chmod', '+x', path_to_installer])
@@ -187,7 +227,7 @@ def install_miniconda(path_to_installer, path_to_install):
 
 
 def setup_miniconda(python_version, channel=None):
-    url = url_for_platform_version(platform, python_version, arch)
+    url = url_for_platform_version(host_platform, python_version, host_arch)
     print('Setting up miniconda from URL {}'.format(url))
     acquire_miniconda(url, miniconda_installer_path)
     install_miniconda(miniconda_installer_path, miniconda_dir)
@@ -250,11 +290,12 @@ def build_upload_and_purge(path, user=None, key=None):
 def upload_and_purge(key, user, channel, filepath):
     print('Uploading to {}/{}'.format(user, channel))
     binstar_upload(key, user, channel, filepath)
-    print('Finished upload, now attempting to login.')
-    filename = p.split(filepath)[-1]
     b = login_with_key(key)
-    print('Logged in. Purging old releases...')
-    purge_old_releases(b, key, user, channel, filename)
+    if channel != 'main':
+        print("Purging old releases from channel '{}'".format(channel))
+        purge_old_files(b, user, channel, filepath)
+    else:
+        print("On main channel - no purging of releases will be done.")
 
 
 def resolve_can_upload_from_travis():
