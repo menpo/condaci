acac734659ba5bd303872e5a98fe0fbfe1f61232
James Booth <jabooth@gmail.com>
2014-09-23 13:50:38 +0100

    remove debug printing

--- traviconda.py
+++ traviconda.py
@@ -435,8 +435,6 @@ def git_head_has_tag():
 
 def setup_cmd(args):
     mc = resolve_mc(args.path)
-    for k, v in os.environ.items():
-        print('{}: {}'.format(k, v))
     setup_miniconda(args.python, mc, channel=args.channel)
 
 
