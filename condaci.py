ec981105c4eeb4a883a360758d062da1e37b28b3
Patrick Snape <patricksnape@gmail.com>
2015-08-18 17:31:23 +0100

    OK - try setting the environ
    This seems to be required, but the system python
    version was coming up incorrect before

--- condaci.py
+++ condaci.py
@@ -281,6 +281,10 @@ def conda_build_package_win(mc, path):
         print('found BINSTAR_KEY in environment on Windows - deleting to '
               'stop vcvarsall from telling the world')
         del os.environ['BINSTAR_KEY']
+    os.environ['PYTHON_ARCH'] = host_arch()[:2]
+    os.environ['PYTHON_VERSION'] = PYTHON_VERSION
+    print('PYTHON_ARCH={} PYTHON_VERSION={}'.format(os.environ['PYTHON_ARCH'],
+                                                    os.environ['PYTHON_VERSION']))
     execute(['cmd', '/E:ON', '/V:ON', '/C', MAGIC_WIN_SCRIPT_PATH,
              conda(mc), 'build', '-q', path])
 
